7f30e763355f97ac83865b28d18de243c9cd1063
harsha-deep <harshadeep.donapati@iiitb.org>
2023-10-28 00:14:49 +0530

    Changed Calculator.java and added logging

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,11 +3,10 @@ package org.example;
 import java.lang.Math;
 import java.util.Scanner;
 
-// import org.apache.logging.log4j.LogManager;
-// import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Calculator {
-
     public static double squareRoot(double n) {
         if (n < 0) {
             System.out.println("Invalid Input");
@@ -22,12 +21,9 @@ public class Calculator {
             System.out.println("Invalid Input");
             return -1;
         }
-
         double result = 1;
-
         for (int i = 2; i <= n; i++)
             result *= i;
-
         return result;
     }
 
@@ -36,22 +32,20 @@ public class Calculator {
             System.out.println("Invalid Input");
             return -1;
         }
-
         double result = Math.log(n);
-
         return result;
     }
 
     public static double exponent(double a, double n) {
         double result = Math.pow(a, n);
-
         return result;
     }
 
-    public static void main(String[] args) {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
 
+    public static void main(String[] args) {
+        logger.info("Start of Execution");
         Scanner sc = new Scanner(System.in);
-        double result, n, exponent;
 
         while (true) {
             System.out.println("1 - Square root");
@@ -60,42 +54,54 @@ public class Calculator {
             System.out.println("4 - Exponential");
             System.out.println("5 - Exit");
 
-            System.out.print("Enter choice: ");
+            System.out.print("Enter Choice: ");
             int choice = sc.nextInt();
 
+            if (choice < 1 || choice > 5) {
+                System.out.println("Enter a valid choice.");
+                logger.warn("Invalid input");
+            }
+
             if (choice == 5) {
+                logger.info("End of Execution");
                 break;
             }
 
-            System.out.print("Enter number: ");
-            n = sc.nextDouble();
+            else {
 
-            if (choice == 1) {
-                result = squareRoot(n);
+                double result, num;
+                System.out.print("Enter number: ");
+                num = sc.nextDouble();
+
+                if (choice == 1) {
+                    logger.info("START OP: SQUARE ROOT");
+                    result = squareRoot(num);
+                    logger.info("END OP: SQUARE ROOT");
+                }
+
+                else if (choice == 2) {
+                    logger.info("START OP: LOGARITHM");
+                    result = log(num);
+                    logger.info("END OP: LOGARITHM");
+                }
+
+                else if (choice == 3) {
+                    logger.info("START OP: FACTORIAL");
+                    result = factorial(num);
+                    logger.info("END OP: FACTORIAL");
+                }
+
+                else {
+                    logger.info("START OP: EXPONENT");
+                    System.out.print("Enter exponent: ");
+                    double exponent = sc.nextDouble();
+                    result = exponent(num, exponent);
+                    logger.info("END OP: EXPONENT");
+                }
+
                 System.out.println("Result: " + result);
                 System.out.println("");
             }
-
-            if (choice == 2) {
-                result = log(n);
-                System.out.println("Result: " + result);
-                System.out.println("");
-            }
-
-            if (choice == 3) {
-                result = factorial(n);
-                System.out.println("Result: " + result);
-                System.out.println("");
-            }
-
-            if (choice == 4) {
-                System.out.print("Enter exponent: ");
-                exponent = sc.nextDouble();
-                result = exponent(n, exponent);
-                System.out.println("Result: " + result);
-                System.out.println("");
-            }
-
         }
 
         if (sc != null)
